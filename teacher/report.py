141b3e7684550eb9056bacc0b2b718eb2b84cf0e
vipul25 <vipul2519@gmail.com>
2015-03-16 00:36:37 +0530

    plagiarism corrected

--- teacher/report.py
+++ teacher/report.py
@@ -20,7 +20,23 @@ if 'HTTP_COOKIE' in os.environ:
 templateLoader = FileSystemLoader( searchpath="/" )
 templateEnv = Environment( loader=templateLoader )
 if c['type'].value == "teacher":
-	contents = open("../course/"+ str(cid) + "/report.txt").read()
+	query="select c.assignment_id,c.problem_id,d.title,a.title,startdate from assignment as a,courses as b,assignment_code as c,problem_code as d where a.course_id=b.course_id and a.type='code' and a.course_id='{0}' and c.assignment_id=a.assignment_id and c.problem_id=d.problem_id".format(str(cid))
+	cursor.execute(query)
+	d=cursor.fetchall()
+	contents='''<br>
+			<div class="panel panel-default">
+			  <!-- Default panel contents -->
+			  <div class="panel-heading">Plagiarism</div>
+			  <div class="panel-body">
+			 <!-- Table -->
+  				<table class="table">
+  			 <tr><th>Assignment Title</th><th>Problem Title</th><th>Date</th><th>View Report</th></tr>'''
+  	for row in d:
+  		contents=contents+'''<tr><td>'''+str(row[3])+'''</td><td><a href="http://www.neoscript.in/plagiarism/3/2/2.txt">'''+str(row[2])+'''</td><td>'''+str(row[4])+'''</a></td><td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-aid="'''+str(row[0])+'''" data-pid="'''+str(row[1])+'''">View Report</button></td></tr>'''
+  	contents=contents+'''</table>
+			</div>
+			</div>'''
+#	contents = open("../course/"+ str(cid) + "/report.txt").read()
 	TEMPLATE_FILE = "/var/www/html/teacher/report.html"
 	template = templateEnv.get_template( TEMPLATE_FILE )
 	templateVars = { "title" : title,  "cid":cid,"name":c['name'].value,"content":contents ,"footer": footer.html }
