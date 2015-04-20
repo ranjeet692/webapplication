554d17558311ee6afa71696aaffa423b59156d8b
Ranjeet Kumar <ranjeetkumar692@gmail.com>
2015-04-21 04:58:34 +0530

    acourse

--- acourse.py
+++ acourse.py
@@ -9,14 +9,7 @@ acourse="select * from courses"
 connection.cursor.execute(acourse)
 all_courses=connection.cursor.fetchall()
 
-user=""
-
-if 'HTTP_COOKIE' in os.environ:
-	cookie_string=os.environ.get('HTTP_COOKIE')
-	c=Cookie.SimpleCookie()
-	c.load(cookie_string)
-	user = c['name'].value
-	
+user=""	
 TEMPLATE_FILE = "/var/www/html/acourse.html" 
 templateVars = { "all_courses":all_courses, "name":user}
 template = templateEnv.get_template( TEMPLATE_FILE )
